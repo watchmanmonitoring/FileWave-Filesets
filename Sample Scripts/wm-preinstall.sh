00961cefb2643ef7c47af130c8e601547ceac59e
Allen Hancock <hancock@yesthatallen.com>
2016-02-03 08:07:56 -0600

    updates to match the readme

--- Sample Scripts/wm-preinstall.sh
+++ Sample Scripts/wm-preinstall.sh
@@ -1,13 +1,9 @@
 #/bin/bash
 
 # Set the Group before installation to prevent excess "Something Changed" emails
-
 /usr/bin/defaults write /Library/MonitoringClient/ClientSettings ClientGroup -string "ENTER_GROUP_NAME_HERE"
-
 
 # Optionally disable auto-updates
 # /usr/bin/defaults write /Library/MonitoringClient/ClientSettings Update_Enabled -bool false
-
-
 
 ## Read more at https://www.watchmanmonitoring.com/terminal-commands
